1ba72c138e276b37d57b750b80892a3b58451396
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-06 09:47:39 -0400

    added uniswap factory interface

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -4,6 +4,20 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
+interface UniswapFactoryInterface {
+    // Public Variables
+    address public exchangeTemplate;
+    uint256 public tokenCount;
+    // Create Exchange
+    function createExchange(address token) external returns (address exchange);
+    // Get Exchange and Token Info
+    function getExchange(address token) external view returns (address exchange);
+    function getToken(address exchange) external view returns (address token);
+    function getTokenWithId(uint256 tokenId) external view returns (address token);
+    // Never use
+    function initializeFactory(address template) external;
+}
+
 contract YourContract {
 
   //event SetPurpose(address sender, string purpose);
